ebe4acfd7b5312a2d5a7435abc026fd274fbd970
D Duu <D Duu@DESKTOP-UV2IARP>
2017-02-17 09:31:45 +0700

    Add project files.

---
+++ javascriptBasic/Controllers/JavaScriptController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace javascriptBasic.Controllers
+{
+    public class JavaScriptController : Controller
+    {
+        // GET: JavaScript
+        public ActionResult initvalue()
+        {
+            return View();
+        }
+    }
+}
---
+++ javascriptBasic/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="javascriptBasic.MvcApplication" Language="C#" %>
